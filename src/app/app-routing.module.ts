43094e486f35a4941fa8c8f377b2e12795d4915d
Edward Yun <nedinsuyun@gmail.com>
2020-03-06 03:46:26 -0400

    Basic settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'people', loadChildren: () => import('./people/people.module').then(m => m.PeopleModule) }, { path: 'planets', loadChildren: () => import('./planets/planets.module').then(m => m.PlanetsModule) }, { path: 'films', loadChildren: () => import('./films/films.module').then(m => m.FilmsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
