11b8f8c5c6aeaf7529483427245812a121f8e4b0
Edward Yun <nedinsuyun@gmail.com>
2020-03-06 14:11:09 -0400

    basic logics with people

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AppService } from '../app.service';
+import { Person } from '../model';
+import * as Repository from '../repository';
 
 @Component({
   selector: 'app-people',
@@ -8,18 +9,70 @@ import { AppService } from '../app.service';
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
+  
+  curPerson: Person;
+  curIndex: number = 0;
 
-  constructor(private http: HttpClient, private appService: AppService) { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
-    this.http.get('https://swapi.co/api/people').subscribe(data =>{
-      console.log(data);
-    });
-    this.appService.getAPI().subscribe(data => {
-      console.log(data);
-    });
-
+    if(Repository.peopleIndex) { this.curIndex = Repository.peopleIndex; }
+    this.getPerson();
   }
 
+  getPerson() {
+    if(Repository.peopleData && Repository.peopleData.length > 0) {
+      let target = Repository.dataFinder("people", null, this.curIndex + 1);
+      if(target) {
+        this.curPerson = target;
+      }
+    }
+    if(!this.curPerson || this.curPerson.getter('_id') != this.curIndex + 1) {
+      this.appService.getAPI("people", this.curIndex + 1).subscribe(data => {
+        if(data) {
+          this.curPerson = Repository.parseJSON(data, "people");
+          Repository.peopleDataAdder(this.curPerson);
+        }
+      });
+    }
+    Repository.valueSetter("peopleIndex", this.curIndex);
+  }
+
+  getTotalPeopleCount() {
+    this.appService.getAPI("people").subscribe(data => {
+      if(data && data["count"]) {
+        Repository.valueSetter("peopleTotal", data["count"]);
+      }
+    });
+  }
+
+  callNextPerson() {
+    if(!Repository.peopleTotal) { this.getTotalPeopleCount() };
+    if(this.curIndex + 1 == Repository.peopleTotal) {
+      this.curIndex = 1;
+    } else {
+      this.curIndex += 1;
+    }
+    this.getPerson();
+  }
+  
+  callPrevPerson() {
+    if(!Repository.peopleTotal) { this.getTotalPeopleCount() };
+    if(this.curIndex == 0) {
+      this.curIndex = Repository.peopleTotal;
+    } else {
+      this.curIndex -= 1;
+    }
+    this.getPerson();
+  }
+
+  genderChecker() {
+    switch(this.curPerson.getter('gender')) {
+      case "male": return "A Man";
+      case "feamle": return "An Woman";
+      case "unknown": return "An Unknown";
+      default: return "Someone";
+    }
+  }
 
 }
---
+++ src/app/repository.ts
@@ -0,0 +1,106 @@
+import { Person, Planet, film } from "./model";
+
+export let peopleData:  Person[];
+export let peopleIndex: number;
+export let peopleTotal: number;
+
+export let planetsData:  Planet[];
+export let planetsIndex: number;
+export let planetsTotal: number
+
+export let filmsData:  film[];
+export let filmsIndex: number;
+export let filmsTotal: number;
+
+export function dataFinder(type: string, url?: string, _id?: number) {
+  let target: any[];
+  switch(type) {
+    case "people": target = peopleData; break;
+    case "planets": target = planetsData; break;
+    case "films": target = filmsData; break;
+    default: return;
+  }
+  let result;
+  try {
+    if(!target) { return null; }
+    if(url && !_id) { _id = getNumber(url); }
+    result = target.find(data => data.getter("_id") === _id);
+  } catch(err) {
+    return null;
+  }
+  return result
+}
+
+export function dataGetter(type: string) {
+  switch(type) {
+    case "people": return peopleData;
+    case "planets": return planetsData;
+    case "films":  return filmsData;
+    default: return null;
+  }
+}
+
+export function peopleDataAdder(person: Person ) {
+  if(!peopleData) { peopleData = new Array<Person>(); }
+  if(!dataFinder("people", person.getter('url'))) {
+    peopleData.push(person);
+  }
+}
+
+export function planetsDataAdder(data: Object) {
+  if(!planetsData || planetsData.length == 0) { planetsData = new Array<Planet>(); }
+  if(!dataFinder("planets", data["url"])) {
+    planetsData.push(parseJSON(data, "planets"));
+  }
+}
+
+export function filmsDataAdder(data: Object) {
+  if(!filmsData) { filmsData = new Array<film>(); }
+  if(!dataFinder("films", data["url"])) {
+     filmsData.push(parseJSON(data, "films"));
+  };
+}
+
+export function indexSetter(type: string, idx: number) {
+  switch(type) {
+    case "people":  peopleIndex = idx; break;
+    case "planets": planetsIndex = idx; break;
+    case "films":  filmsIndex  = idx; break;
+    default: break;
+  }
+}
+
+export function parseJSON(data: Object, type: string) {
+  let keys = Object.keys(data);
+  let obj;
+  switch(type) {
+    case "people":  obj = new Person(); break;
+    case "planets": obj = new Planet(); break;
+    case "films":  obj = new film(); break;
+    default: break;
+  }
+  for(let key of keys) {
+    obj.setter(key, data[key]);
+    obj.setter("_id", getNumber(data["url"]));
+  }
+  return obj;
+}
+
+export function getNumber(string: string) {
+  if(string.substr(string.length -1, string.length) == '/') {
+    string = string.substr(0, string.length -1);
+ }
+  return parseInt(string.match(/([^\/]+$)/)[1]);
+}
+
+export function valueSetter(key: string, value: number) {
+  switch(key) {
+    case "peopleIndex":   peopleIndex = value;  break;
+    case "peopleTotal":   peopleTotal = value;  break;
+    case "planetsIndex":  planetsIndex = value; break;
+    case "planetsTotal":  planetsTotal = value; break;
+    case "filmsIndex":   filmsIndex = value;  break;
+    case "filmsTotal":   filmsTotal = value;  break;
+    default: break;
+  }
+}
