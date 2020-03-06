284fbb4b3d6e55cc4fc7394c926ffdb22372e5c9
Edward Yun <nedinsuyun@gmail.com>
2020-03-06 18:51:52 -0400

    planet logic updates

--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+import * as Repository from '../repository';
+import { Planet } from '../model';
 
 @Component({
   selector: 'app-planets',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetsComponent implements OnInit {
 
-  constructor() { }
+  curPlanets: Planet[];
+  total: number;
+  searchStr: string;
+
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
+    if(Repository.planetsTotal) { this.total = Repository.planetsTotal; }
+    if(!Repository.planetsData || Repository.planetsData.length == 0) {
+      this.appService.getAPI("planets").subscribe(planetsData => {
+        if(planetsData) {
+          Repository.valueSetter("planetsTotal", planetsData["count"]);
+          this.total = Repository.planetsTotal;
+          if(!Repository.planetsTotal) { Repository.valueSetter("planetsTotal", planetsData["count"]); }
+          planetsData["results"].map(planet => {
+            Repository.planetsDataAdder(planet);
+          });
+          this.curPlanets = Repository.planetsData;
+        }
+      });
+    }
   }
+
+  spellChecker(name: string) {
+    let result = "";
+    switch(name.substr(0,1)) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u": result += "An "; break;
+      default: result += "A "; break;
+    }
+    result += name + " Planet"
+    return result
+  }
+
+
+  // /**
+  //  * Search starts from 2nd character
+  //  */
+  // planetFilter() {
+  //   if(!this.searchStr || this.searchStr.length < 2) {
+  //     return this.allPlanets;
+  //   } else {
+  //     return this.allPlanets.filter(planet => 
+  //       JSON.stringify(planet).toLowerCase().search(this.searchStr.toLowerCase()) != -1
+  //     );
+  //   }
+  // }
 
 }
