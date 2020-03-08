723e85d418e38bb8b2d277d037dbebc20d26151e
Edward Yun <nedinsuyun@gmail.com>
2020-03-08 13:39:33 -0300

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   
-  title = 'SWExplorer';
+  title = 'Star Wars Explorer';
   
   constructor(public spinner: SpinnerService, private router: Router) { }
 
